76f106ff77b5ffe6ee85e85861b34a4d31ff3a7a
FemFX <ignedko34@gmail.com>
2019-09-22 17:59:41 +0300

    Head

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <header class="bg-secondary text-center p-3 mb-5">
+    <h1>WordBeater</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
