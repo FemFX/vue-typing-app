bd2d70f5c593200a047f8604b1223713e6eeed62
FemFX <ignedko34@gmail.com>
2019-09-22 18:00:08 +0300

    App

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <Header />
+    <Typing/>
+    <Instruction />
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header.vue";
+import Typing from "@/components/Typing.vue";
+import Instruction from "@/components/Instruction.vue";
+export default {
+  name: "app",
+  components: {
+    Header,
+    Typing,
+    Instruction
+  },
+  data() {
+    return {
+      
+    };
+  }
+};
+</script>
+
+<style>
+body {
+  background: #6c757d;
+  color: #fff;
+}
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+</style>
