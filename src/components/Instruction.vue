cf6402f3010168f5755dc8190e18a70a7580e42e
FemFX <ignedko34@gmail.com>
2019-09-22 17:59:56 +0300

    Instruction

---
+++ src/components/Instruction.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="row mt-5">
+    <div class="col-md-12">
+      <div class="card card-body bg-secondary text-white">
+        <h5>Instructions</h5>
+        <p>
+          Type each word in the given amount of seconds to score. To play again, just type the current word. Your score
+          will reset
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Instruction"
+};
+</script>
+<style>
+
+</style>
